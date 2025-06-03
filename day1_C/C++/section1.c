ef3b667cc59573c740bab09957c35a8327e26b6a
Sayal Singh <sayalgmr@gmail.com>
2025-06-03 11:57:01 +0530

    Swapping without third variable

--- day1_C/C++/section1.c
+++ day1_C/C++/section1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Hello World!");
-  return 0;
+    int a, b;
+    printf("Enter first char: ");
+    scanf("%d", &a);
+    printf("Enter second char: ");
+    scanf(" %d", &b); // Note the space before %c to consume any leftover newline character
+    printf("You entered: %d and %d\n", a, b);
+
+    a = a + b;
+    b = a - b;
+    a = a-b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
 }
