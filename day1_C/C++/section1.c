69b483c776713542e5ce3d26f3b24c7ec420b4a7
Sayal Singh <sayalgmr@gmail.com>
2025-06-03 11:23:22 +0530

    Comments added

--- day1_C/C++/section1.c
+++ day1_C/C++/section1.c
@@ -4,3 +4,6 @@ int main() {
   printf("Hello World!");
   return 0;
 }
+// This code prints "Hello World!" to the console.
+// It includes the standard input-output header file and defines the main function.
+// The main function is the entry point of the program.
