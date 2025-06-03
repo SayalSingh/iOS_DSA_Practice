34a4f870051aa6a16bcba25ec794050b7b3ce3e9
Sayal Singh <sayalgmr@gmail.com>
2025-06-03 11:26:21 +0530

    first commit

--- day1_C/C++/section1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("Hello World!");
-  return 0;
-}
-// This code prints "Hello World!" to the console.
-// It includes the standard input-output header file and defines the main function.
-// The main function is the entry point of the program.
