d044ed63157e38587ebe96281bcb1b2338269456
Sayal Singh <sayalgmr@gmail.com>
2025-06-04 12:41:20 +0530

    Question 2 done

--- day2_C/C++/section4.c
+++ day2_C/C++/section4.c
@@ -33,3 +33,43 @@ int main() {
 
 
 */
+
+/* Question 2
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int a,b;
+    printf("Enter no. of rows : ");
+    scanf("%d",&a);
+    printf("Enter no. of columns : ");
+    scanf("%d",&b);
+    
+    char arr[a][b];
+    int i,j;
+    for (i = 0; i < a;i++) {
+        for (j = 0; j < b; j++) {
+            printf("Enter the arrays for [%d][%d]: ", i,j);
+            scanf(" %c",&arr[i][j]);
+        }
+    }
+    printf("The array is as following : \n");
+    for (i = 0; i < a;i++) {
+        for (j = 0; j < b; j++) {
+            printf("%c ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+       for (i = 0; i < a;i++) {
+        for (j = 0; j < b; j++) {
+        }
+        printf("Size of row %d is : %d \n", i,b);
+    }
+   
+   return 0;
+   
+}
+
+*/
