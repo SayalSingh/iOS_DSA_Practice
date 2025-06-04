6216a78a759641ae5cef1f0bf49501c40fa6e3de
Sayal Singh <sayalgmr@gmail.com>
2025-06-04 10:45:42 +0530

    Question 1 done_S3

---
+++ day2_C/C++/section3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Question 1
+int main() {
+    int a, i, fact = 1;
+    printf("Enter the no. you need factorial: ");
+    scanf("%d", &a);
+    for (i= 1; i <= a; i++) {
+        fact *= i;
+    }
+    printf("%d",fact);
+    
+
+    return 0;
+}
+
+*/
