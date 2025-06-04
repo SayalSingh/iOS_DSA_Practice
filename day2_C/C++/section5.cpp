a15b9aece090d5501bad7ce9add5569ac57f35d5
Sayal Singh <sayalgmr@gmail.com>
2025-06-04 15:37:06 +0530

    Section 5 started Question 1 done_S5

---
+++ day2_C/C++/section5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+/* Question 1
+
+int main() {
+    string str1;
+    str1 = "Hello C++!";
+    cout << "String : " << str1 << endl;
+    cout <<  "Size of String : " << str1.length() << endl;
+    cout << "Seventh Index Element : ' " << str1[7] << " '" << endl;
+}
+
+*/
+
+/* Question 2
+*/
