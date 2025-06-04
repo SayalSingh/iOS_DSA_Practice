0409b3526e7ad638bf78d7eeac4ef4fbc492f7e3
Sayal Singh <sayalgmr@gmail.com>
2025-06-04 13:45:39 +0530

    Question 3 done

--- day2_C/C++/section4.c
+++ day2_C/C++/section4.c
@@ -73,3 +73,35 @@ int main() {
 }
 
 */
+
+
+/* Question 3
+#include <string.h>
+
+int main() {
+    
+    char str1[100], str2[100], result[100];
+    printf("Enter String 1 : ");
+    scanf(" %c", &str1);
+    printf("Enter String 2 : ");
+    scanf(" %c", &str2);
+
+
+    int i, j;
+    
+    for (i = 0; str1[i] != '\0'; i++) {
+        result[i] = str1[i];
+    }
+    
+    for (j = 0; str2[j] != '\0'; j++) {
+        result[i + j] = str2[j];
+    }
+    
+    result[i + j] = '\0';
+    
+    printf("Concatenated string: %s", result);
+ 
+   return 0;
+   
+}
+*/
